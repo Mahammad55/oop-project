15b77efbde6972bd1d4bb364779d7852e455bf4c
Mahammad Ilyazov <ilyazovmehemmed@gmail.com>
2024-01-08 10:49:18 +0400

    Xiaomi Run Calculator method added

--- src/calculatorapplication/classes/XiaomiCalculatorImpl.java
+++ src/calculatorapplication/classes/XiaomiCalculatorImpl.java
@@ -5,6 +5,31 @@ import calculatorapplication.interfaces.XiaomiCalculator;
 import calculatorapplication.method.Method;
 
 public class XiaomiCalculatorImpl extends AbstractCalculator implements XiaomiCalculator {
+    public static void runCalculator() {
+        System.out.println("Xiaomi runs.");
+
+        String[] listOfOperations = Method.loadOperations(4);
+
+        System.out.println("Əməliyyatı seçin.");
+        for (int i = 0; i < listOfOperations.length; i++) {
+            System.out.println(i + 1 + ": " + listOfOperations[i]);
+        }
+
+        int operationNumber = Method.numRangePicker(1, listOfOperations.length);
+
+        XiaomiCalculator calculator=new XiaomiCalculatorImpl();
+
+        switch (operationNumber) {
+            case 1 -> calculator.sum();
+            case 2 -> calculator.subtraction();
+            case 3 -> calculator.multiply();
+            case 4 -> calculator.divide();
+            case 5 -> calculator.round();
+            case 6 -> calculator.roundFloor();
+            case 7 -> calculator.roundCeil();
+        }
+    }
+
     @Override
     public void round() {
         System.out.print("Ədədi daxil edin: ");
