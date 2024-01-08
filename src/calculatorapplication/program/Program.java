abab35a69f4e384401665455245aee180bd3b58d
Mahammad Ilyazov <ilyazovmehemmed@gmail.com>
2024-01-08 10:41:48 +0400

    Program class and main method added

---
+++ src/calculatorapplication/program/Program.java
@@ -0,0 +1,12 @@
+package calculatorapplication.program;
+
+public class Program {
+    public static void main(String[] args) {
+        System.out.println("""
+                Aşağıdakı 4 kalkulyatordan birini seçin.
+                1. Apple Calculator
+                2. Huawei Calculator
+                3. Samsung Calculator
+                4. Xiaomi Calculator""");
+    }
+}
