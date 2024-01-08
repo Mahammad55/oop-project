236869f843b31e6c31d32b64154b1e81b6c22467
Mahammad Ilyazov <ilyazovmehemmed@gmail.com>
2024-01-08 10:47:21 +0400

    Apple Run Calculator method added

--- src/calculatorapplication/classes/AppleCalculatorImpl.java
+++ src/calculatorapplication/classes/AppleCalculatorImpl.java
@@ -5,6 +5,30 @@ import calculatorapplication.interfaces.AppleCalculator;
 import calculatorapplication.method.Method;
 
 public class AppleCalculatorImpl extends AbstractCalculator implements AppleCalculator {
+    public static void runCalculator() {
+        System.out.println("Apple runs.");
+
+        String[] listOfOperations = Method.loadOperations(1);
+
+        System.out.println("Əməliyyatı seçin.");
+        for (int i = 0; i < listOfOperations.length; i++) {
+            System.out.println(i + 1 + ": " + listOfOperations[i]);
+        }
+
+        int operationNumber = Method.numRangePicker(1, listOfOperations.length);
+
+        AppleCalculator calculator = new AppleCalculatorImpl();
+
+        switch (operationNumber) {
+            case 1 -> calculator.sum();
+            case 2 -> calculator.subtraction();
+            case 3 -> calculator.multiply();
+            case 4 -> calculator.divide();
+            case 5 -> calculator.power();
+            case 6 -> calculator.squareRoot();
+        }
+    }
+
     @Override
     public void power() {
         System.out.print("İlk ədədi daxil edin: ");
