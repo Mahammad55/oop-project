4483e8ba42e08cf4bc829a37e3360ac968a3c534
Mahammad Ilyazov <ilyazovmehemmed@gmail.com>
2024-01-08 10:31:33 +0400

    AbstractCalculator class and methods added

---
+++ src/calculatorapplication/classes/AbstractCalculator.java
@@ -0,0 +1,37 @@
+package calculatorapplication.classes;
+
+import calculatorapplication.method.Method;
+
+public abstract class AbstractCalculator {
+    public void sum() {
+        System.out.print("İlk ədədi daxil edin: ");
+        double num1 = Method.numPicker();
+        System.out.print("İkinci ədədi daxil edin: ");
+        double num2 = Method.numPicker();
+        System.out.printf("%s + %s = %s\n", num1, num2, num1 + num2);
+    }
+
+    public void subtraction() {
+        System.out.print("İlk ədədi daxil edin: ");
+        double num1 = Method.numPicker();
+        System.out.print("İkinci ədədi daxil edin: ");
+        double num2 = Method.numPicker();
+        System.out.printf("%s - %s = %s\n", num1, num2, num1 - num2);
+    }
+
+    public void multiply() {
+        System.out.print("İlk ədədi daxil edin: ");
+        double num1 = Method.numPicker();
+        System.out.print("İkinci ədədi daxil edin: ");
+        double num2 = Method.numPicker();
+        System.out.printf("%s * %s = %s\n", num1, num2, num1 * num2);
+    }
+
+    public void divide() {
+        System.out.print("İlk ədədi daxil edin: ");
+        double num1 = Method.numPicker();
+        System.out.print("İkinci ədədi daxil edin: ");
+        double num2 = Method.numPicker();
+        System.out.printf("%s / %s = %s\n", num1, num2, num1 / num2);
+    }
+}
